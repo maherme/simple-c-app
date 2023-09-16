0ccc9ad39c2bc0ffebab2ae27477abf9da09b83e
maherme <manuel.hernandez.mendez@gmail.com>
2023-09-16 16:04:28 +0200

    Add main.c file

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    printf("%s: Hello World from a source file in a git repository\n", argv[0]);
+    exit(EXIT_SUCCESS);
+}
